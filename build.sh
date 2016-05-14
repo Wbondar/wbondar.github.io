42a719605051f67fd1e2c8b2dbfa36a8eabd3b23
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2016-05-14 18:39:35 +0200

    v1

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-#cd "$(dirname "$0")" # Go to the script's directory
-source mo
-source $1
-cat template.mo | mo > $2
--- default-build.sh
+++
@@ -1,3 +0,0 @@
-source build.sh en.conf index.html
-source build.sh pl.conf pl.html
-source build.sh ru.conf ru.html
